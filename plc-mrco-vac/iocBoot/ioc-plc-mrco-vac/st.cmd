bde1c7a5c8f82f272c9486e9aff9e9450bdc48f2
Maarten Thomas-Bosum <bosum123@psdev03.slac.stanford.edu>
2021-10-28 17:12:40 -0700

    Committing most up to date running IOC

--- plc-mrco-vac/iocBoot/ioc-plc-mrco-vac/st.cmd
+++ plc-mrco-vac/iocBoot/ioc-plc-mrco-vac/st.cmd
@@ -5,7 +5,7 @@
 
 epicsEnvSet("ADS_IOC_TOP", "$(TOP)" )
 
-epicsEnvSet("ENGINEER", "payers" )
+epicsEnvSet("ENGINEER", "adpai" )
 epicsEnvSet("LOCATION", "PLC:MRCO:VAC" )
 epicsEnvSet("IOCSH_PS1", "$(IOC)> " )
 epicsEnvSet("ACF_FILE", "$(ADS_IOC_TOP)/iocBoot/templates/unrestricted.acf")
